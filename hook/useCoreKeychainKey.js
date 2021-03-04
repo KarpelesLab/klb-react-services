1ec6e663d6578d8e020765d2600d743ad1ca2092
Spirits <matthieu.bastianel@gmail.com>
2021-03-04 14:45:04 +0900

    feat: stuff

--- hook/useCoreKeychainKey.js
+++ hook/useCoreKeychainKey.js
@@ -2,9 +2,9 @@ import { useAction, useResourceList } from './useBaseHooks';
 import { useCallback }                from 'react';
 
 export const useCoreKeychainKeys = keychainId => useResourceList(`Keychain/${keychainId}/Key`);
-export const useCoreKeychainKeyDelete = keychainKeyId => useAction(`Keychain/Key/${keychainKeyId}`, 'DELETE', {snackMessageToken: 'key_delete_success'});
+export const useCoreKeychainKeyDelete = keychainKeyId => useAction(`Keychain/Key/${keychainKeyId}`, 'DELETE', { snackMessageToken: 'key_delete_success' });
 export const useCoreKeychainKeyCreate = keychainId => {
-	const [_doAction, loading] = useAction(`Keychain/${keychainId}/Key:create`, 'POST', {snackMessageToken:'key_create_success'});
+	const [_doAction, loading] = useAction(`Keychain/${keychainId}/Key:create`, 'POST', { snackMessageToken: 'key_create_success' });
 
 	const doAction = useCallback((label, publicKey, expires) => _doAction({
 		label: label,
