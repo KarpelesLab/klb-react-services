9e99a236ccaa1e405a8fa6d343603572d822c8d8
Spirits <matthieu.bastianel@gmail.com>
2021-03-04 13:58:02 +0900

    feat: export base api handlers

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ export * from './hook/useCoreAffiliate'
 export * from './hook/useCoreOAuth'
 export * from './hook/useCoreKeychainKey'
 export * from './hook/useCoreKeychain'
+
+export * from './hook/useBaseHooks'
+export * from './hook/useApiErrorHandler'
+
 export * from './context/RestContext'
