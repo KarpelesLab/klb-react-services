f8b4a8e777c868ea784b64ecff2262098174fee1
Spirits <matthieu.bastianel@gmail.com>
2021-03-04 17:31:52 +0900

    feat: testing stuff

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-export { RestContextContainer, RestContext, defaultRestContext }                           from './context/RestContext';
 export { useCoreAffiliate, useCoreUserWallets }                                            from './hook/useCoreAffiliate';
 export { useCoreFetchConsumerLink, useCoreUnlinkToken, useCoreOAuth2ConsumerTokens }       from './hook/useCoreOAuth';
 export { useCoreKeychainKeys, useCoreKeychainKeyDelete, useCoreKeychainKeyCreate }         from './hook/useCoreKeychainKey';
 export { useCoreKeychains, useCoreKeychain, useCoreKeychainUpdate, useCoreKeychainCreate } from './hook/useCoreKeychain';
 export { useResource, useResourceList, useAction }                                         from './hook/useBaseHooks';
 export { useApiErrorHandler }                                                              from './hook/useApiErrorHandler';
+export { RestContextContainer, RestContext, defaultRestContext }                           from './context/RestContext';
